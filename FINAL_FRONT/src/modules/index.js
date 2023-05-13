bc993d6a3f1e226eb1b2bb70ddf49417f4652363
yujunglove <dbwjdeor1234562@gmail.com>
2023-05-13 15:10:55 +0900

    commit

--- FINAL_FRONT/src/modules/index.js
+++ FINAL_FRONT/src/modules/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import memberReducer from "./MemberModule";
 import myPageReducer from "./MyPageModule";
+import noticeReducer from "./NoticeModule";
+import equipmentReducer from "./EquipmentModule";
 
 const rootReducer = combineReducers({
     memberReducer,
-    myPageReducer
+    myPageReducer,  
+     noticeReducer,
+     equipmentReducer
 });
 
 export default rootReducer;
