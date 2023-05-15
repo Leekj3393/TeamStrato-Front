24ab1680492ad3fc99f980621f08e77ce45b5d37
ehyunseok <yhdaneys@gmail.com>
2023-05-15 09:18:17 +0900

    ㅏㅓㅏㅏ

--- FINAL_FRONT/src/pages/approval/ApprovalLineRegist.js
+++ FINAL_FRONT/src/pages/approval/ApprovalLineRegist.js
@@ -5,11 +5,16 @@ import ApprovalCSS from './Approval.module.css';
 function ApprovalLineRegist() {
     return(
         <div className={ApprovalCSS}>
-            <div className={ApprovalCSS.content0}>
-                전자결재 결재선 선택 페이지!
+            <div className={ApprovalCSS.square}></div>                  {/* 본문 하얀 네모 */}
+            <div className={ApprovalCSS.content0}>                      {/* 본문 내용 시작 */}
+                <div className={ApprovalCSS.contentDiv}>
+                    기안문 작성 &gt; <b>결재선/열람자 선택</b> &gt; 결재 요청
+                </div>
+                
 
 
-            </div>
+
+        </div>                                                          {/* 본문 내용 끝  */}
         </div>
     );
 }
