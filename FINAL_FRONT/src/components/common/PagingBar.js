616d2ca9d536fa8229f6442c21ab78a531a60991
ehyunseok <yhdaneys@gmail.com>
2023-05-16 11:28:31 +0900

    이현-페이징바 하나로 통일했섬니당

--- FINAL_FRONT/src/components/common/PagingBar.js
+++ FINAL_FRONT/src/components/common/PagingBar.js
@@ -10,46 +10,48 @@ function PagingBar({ pageInfo, setCurrentPage }) {
     }
 
     return (
-        <div  className ={PagingBarCSS.memberPaging}>
-            <button 
-                className={ PagingBarCSS.pagingBtn }
-                onClick={ () => setCurrentPage(1) }
-                disabled={ pageInfo.currentPage <= 1 }
-            >
-                &lt;&lt;
-            </button>
-            <button 
-                className={ PagingBarCSS.pagingBtn }
-                onClick={ () => setCurrentPage(pageInfo.currentPage - 1) }
-                disabled={ pageInfo.currentPage <= 1 }
-            >
+        <div className={PagingBarCSS}>
+            <div className={PagingBarCSS.noticeDiv}>
+                <button 
+                    className={ PagingBarCSS.pagingBtn }
+                    onClick={ () => setCurrentPage(1) }
+                    disabled={ pageInfo.currentPage <= 1 }
+                >
+                    &lt;&lt;
+                </button>
+                <button 
+                    className={ PagingBarCSS.pagingBtn }
+                    onClick={ () => setCurrentPage(pageInfo.currentPage - 1) }
+                    disabled={ pageInfo.currentPage <= 1 }
+                >
                 &lt;
-            </button>
-            { pageNumber.map(num => (
-                <li key={num} onClick={ () => setCurrentPage(num) }>
-                    <button 
-                        className={ PagingBarCSS.pagingBtn }
-                        style={ pageInfo.currentPage === num ? { backgroundColor : 'orange' } : null }
-                    >
-                        {num}
-                    </button>
-                </li>
-            ))
-            }
-            <button 
-                className={ PagingBarCSS.pagingBtn }
-                onClick={ () => setCurrentPage(pageInfo.currentPage + 1) }
-                disabled={ pageInfo.currentPage <= pageInfo.maxPage }
-            >
-                &gt;
-            </button>
-            <button 
-                className={PagingBarCSS.pagingBtn}
-                onClick={() => setCurrentPage(pageInfo.maxPage)}
-                disabled={pageInfo.currentPage <= pageInfo.maxPage}
-            >
-                &gt;&gt;
-            </button>
+                </button>
+                { pageNumber.map(num => (
+                    <li key={num} onClick={ () => setCurrentPage(num) }>
+                        <button 
+                            className={ PagingBarCSS.pagingBtn }
+                            style={ pageInfo.currentPage === num ? { backgroundColor : 'lightblue' } : null }
+                        >
+                            {num}
+                        </button>
+                    </li>
+                ))
+                }
+                <button 
+                    className={ PagingBarCSS.pagingBtn }
+                    onClick={ () => setCurrentPage(pageInfo.currentPage + 1) }
+                    disabled={ pageInfo.currentPage <= pageInfo.maxPage }
+                >
+                    &gt;
+                </button>
+                <button 
+                    className={PagingBarCSS.pagingBtn}
+                    onClick={() => setCurrentPage(pageInfo.maxPage)}
+                    disabled={pageInfo.currentPage <= pageInfo.maxPage}
+                >
+                    &gt;&gt;
+                </button>
+            </div>
 
         </div>
     );
