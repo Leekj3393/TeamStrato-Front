1cd50233f9f079d2b92345649e2dae8e550ec6aa
KJlee <lkj9676@gmail.com>
2023-05-18 17:30:54 +0900

    임시비밀번호 발송  & 변경 구현

--- FINAL_FRONT/src/apis/MailAPICalls.js
+++ FINAL_FRONT/src/apis/MailAPICalls.js
@@ -1,13 +1,15 @@
+import { Navigate } from "react-router-dom";
 import { postMail } from "../modules/MailModule";
 
 const SERVER_IP = `${process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const SERVER_PORT = `${process.env.REACT_APP_RESTAPI_SERVER_PORT}`;
-const PRE_URL = `http://${SERVER_IP}:${SERVER_PORT}/skylift/member`;
+const PRE_URL = `http://${SERVER_IP}:${SERVER_PORT}`;
 
 
 export const callMailAPI = (form) => {
+    console.log('callMailAPI form', form);
 
-    const requestURL = `${PRE_URL}/login/findpwd`;
+    const requestURL = `${PRE_URL}/auth/send`;
 
     return async (dispatch, getState) => {
 
@@ -22,45 +24,10 @@ export const callMailAPI = (form) => {
 
         console.log('[MaillAPICalls] callMAilAPI result : ', result);
 
-        if(result.status === 200){
-
-        }
-
-        dispatch(postMail(result));
-        
+        if(result.status === 404/* 백단 return값 수정해서 코드 200 되면 200으로 수정할게요 */){
+            dispatch(postMail(result));
+            alert("메일이 전송 되었습니다. 확인 후 비밀번호를 변경해주세요.");
+        } else {
+        alert("입력하신 정보의 아이디가 없습니다.."); }
     }
 }
-
-
-
-// // 이메일 인증번호
-// const onClickFindPwdHandler = () => {
-//     ajax({
-//        type : "POST",
-//        url : "login/findPwd",
-//        data : {
-//           "email" : mail.val()
-//        },
-//        success : function(data){
-//           alert("해당 이메일로 인증번호 발송이 완료되었습니다. \n 확인부탁드립니다.")
-//           console.log("data : "+ data);
-//           chkEmailConfirm(data, mailconfirm, mailconfirmTxt);
-//        }
-//     })
-//  }
- 
-//      // 이메일 인증번호 체크 함수
-//      function chkEmailConfirm(data, mailconfirm, mailconfirmTxt){
-//          mailconfirm.on("keyup", function(){
-//              if (data != mailconfirm.val()) { //
-//                  confirmchk = false;
-//                  mailconfirmTxt.html("<span id='emconfirmchk'>인증번호가 잘못되었습니다</span>")
-                 
-//                  //console.log("중복아이디");
-//              } else { // 아니면 중복아님
-//                  emconfirmchk = true;
-//                  mailconfirmTxt.html("<span id='emconfirmchk'>인증번호 확인 완료</span>")
- 
-//              }
-//          })
-//      }
