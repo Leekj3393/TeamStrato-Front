78f5d35b33c1a83cdf7299d3fd39bf719a435262
ehyunseok <yhdaneys@gmail.com>
2023-05-19 17:52:05 +0900

    이현-결재등록 구현중~~!!!

--- FINAL_FRONT/src/apis/ApprovalAPICalls.js
+++ FINAL_FRONT/src/apis/ApprovalAPICalls.js
@@ -1,9 +1,30 @@
-import { postApproval } from "../modules/ApprovalModule";
+import { getAppMemberInfo, postApproval } from "../modules/ApprovalModule";
+
 
 const SERVER_IP = `${process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const SERVER_PORT = `${process.env.REACT_APP_RESTAPI_SERVER_PORT}`;
 const PRE_URL = `http://${SERVER_IP}:${SERVER_PORT}/skylift/approval`;
 
+// 결재자 정보 불러오기
+//로그인이 완료된 후 회원 정보를 서버에 요청하는 함수
+export const callApprovalMemberInfoAPI = () => {
+    const requestURL = `${PRE_URL}/memberInfo`;
+
+    return async (dispatch, getState) => {
+        const result = await fetch(requestURL, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        }).then(response => response.json());
+
+        if (result.status === 200) {
+            dispatch(getAppMemberInfo(result));
+            console.log('result', result);
+        }
+    }
+}
 
 // 결재문서 등록
 export const callApprovalRegistAPI = (form) => {
--- FINAL_FRONT/src/modules/ApprovalModule.js
+++ FINAL_FRONT/src/modules/ApprovalModule.js
@@ -1,17 +1,21 @@
 import { createActions, handleActions } from "redux-actions";
 
-const initialState = [];
+const initialState = {};
 
+
+const GET_APP_MEMBER_INFO = 'approval/GET_APP_MEMBER_INFO';
 const POST_APPROVAL = 'approval/POST_APPROVAL';
 
 
-export const { approval : { postApproval}} = createActions({
+export const { approval : { getAppMemberInfo, postApproval}} = createActions({
+    [GET_APP_MEMBER_INFO] : res => res.data,
     [POST_APPROVAL] : res => res,
 });
 
 
 const approvalReducer = handleActions (
     {
+        [GET_APP_MEMBER_INFO] : (state, {payload}) => ({appMember: payload}),
         [POST_APPROVAL] : (state, {payload}) => ({regist: payload}),
     }
 , initialState);
