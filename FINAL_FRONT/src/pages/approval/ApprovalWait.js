1531a748cfdf86d5c5f807763e6574f021dcf817
ehyunseok <yhdaneys@gmail.com>
2023-05-20 21:34:04 +0900

    이현-풀하려고 푸시!!

--- FINAL_FRONT/src/pages/approval/ApprovalWait.js
+++ FINAL_FRONT/src/pages/approval/ApprovalWait.js
@@ -4,8 +4,12 @@ import ApprovalCSS from './Approval.module.css';
 function Approval() {
     return(
         <div className={ApprovalCSS}>
-            <div className={ApprovalCSS.content0}>
+            <div className={ApprovalCSS.square}></div>
+            <div className={ApprovalCSS.appContentDiv}>
                 전자결재 결재 대기함!
+                <div className={ApprovalCSS.appWListTableInfo}>
+                    <h3>페이지 1 / 100 || 총 문서수 500 개 </h3> {/* 수정하기 count */}
+                </div>
             </div>
         </div>
     );
