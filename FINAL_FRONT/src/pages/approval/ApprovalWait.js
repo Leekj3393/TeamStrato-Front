7ab648d06ba453b07ec848af5afaed5972fb3e56
ehyunseok <yhdaneys@gmail.com>
2023-05-21 15:34:04 +0900

    이현-페이징바 오류 수정, 공지사항 날짜 표시 수정, 전자결재 목록 구현중..

--- FINAL_FRONT/src/pages/approval/ApprovalWait.js
+++ FINAL_FRONT/src/pages/approval/ApprovalWait.js
@@ -12,6 +12,14 @@ function Approval() {
     const pageInfo = approvals.pageInfo;
     const [currentPage, setCurrentPage] = useState(1);
 
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}년 ${month}월 ${day}일`;
+    }
+
     useEffect(() => {
         dispatch(callApprovalWListAPI({currentPage}));
     }, 
@@ -42,7 +50,7 @@ function Approval() {
                                 <tr className={ApprovalCSS.lists} key={approval.appCode}>
                                     <th>{approval.appCode}</th>
                                     <th>{approval.appType}</th>
-                                    <th>{approval.appRegistDate}</th>
+                                    <th>{formatDate(approval.appRegistDate)}</th>
                                     <th>{approval.appTitle}</th>
                                     <th>{approval.appStatus}</th>
                                     <th>{/* {approval.member.department.deptName}-{approval.member.memberName} */}</th>
