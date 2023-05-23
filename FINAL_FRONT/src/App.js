bba19cb6b8445365c8f212b096a04d9495ca45b7
ehyunseok <yhdaneys@gmail.com>
2023-05-23 17:17:35 +0900

    이현- 결재선 조직도 구현중

--- FINAL_FRONT/src/App.js
+++ FINAL_FRONT/src/App.js
@@ -43,6 +43,7 @@ import EducationSubLayout from "./layouts/EducationSubLayout";
 import Education from "./pages/education/Education";
 import PartNoticeContent from "./pages/myPage/PartNoticeContent";
 import NoticeRegist from "./pages/notice/NoticeRegist";
+import NestedList from "./pages/approval/NestedList";
 
 function App() {
   return (
@@ -85,6 +86,7 @@ function App() {
           <Route path="/approval" element={<ApprovalSubLayout />}>
             <Route index element={<Approval />} />
             <Route path="/approval/regist" element={<ApprovalRegist />} />
+            <Route path="/approval/toggletestpage" element={<NestedList />} />  {/* 테스트 용!! */}
             <Route path="/approval/appline" element={<ApprovalLineRegist />} />
             <Route path="/approval/wait" element={<ApprovalWait />} />
             <Route path="/approval/inProgress" element={<ApprovalInProgress />} />            
