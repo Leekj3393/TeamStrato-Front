d9335801880a84346ebe72bb15d9787a07e7a682
ehyunseok <yhdaneys@gmail.com>
2023-05-23 17:56:49 +0900

    이현-집에가려고 푸시

--- FINAL_FRONT/src/pages/approval/NestedList.js
+++ FINAL_FRONT/src/pages/approval/NestedList.js
@@ -5,14 +5,17 @@ import ApprovalCSS from './Approval.module.css';
 
 function NestedList() {
   const [expandedItems, setExpandedItems] = useState([]);
-  const {data, accessors} = useSelector(state => state.applineReducer);
+  const { data, accessors } = useSelector(state => state.applineReducer);
   const dispatch = useDispatch();
 
-  //부서에 해당하는 직원 조회
-  const renderMembers = (dept) => {
+  // 부서에 해당하는 동일 직급 직원 조회
+  const renderMembers = (dept, jobName) => {
     if (!accessors) return null; // data가 undefined인 경우 처리
 
-    const members = accessors.accessor.filter(accessor => accessor.department.deptCode === dept.deptCode);
+    const members = accessors.accessor.filter(accessor => (
+      accessor.department.deptCode === dept.deptCode &&
+      accessor.job.jobName === jobName
+    ));
 
     if (members.length === 0) return null;
 
@@ -20,36 +23,54 @@ function NestedList() {
       <ul>
         {members.map(member => (
           <li key={member.memberCode}>
-            {member.job.jobName} - 
-            {member.memberName}
-            {renderMembers(member)} {/* 재귀적으로 하위 멤버 출력 */}
+            {member.job.jobName} - {member.memberName}
           </li>
         ))}
       </ul>
     );
   };
 
+  const handleItemClick = (dept, jobName) => {
+    const item = { dept, jobName };
+    if (expandedItems.some(expandedItem => (
+      expandedItem.dept.deptCode === dept.deptCode &&
+      expandedItem.jobName === jobName
+    ))) {
+      setExpandedItems(expandedItems.filter(expandedItem => (
+        expandedItem.dept.deptCode !== dept.deptCode ||
+        expandedItem.jobName !== jobName
+      )));
+    } else {
+      setExpandedItems([...expandedItems, item]);
+    }
+  };
+
   useEffect(() => {
-      dispatch(callMemberListForAppAPI());
-    }, 
-    []
-  );
+    dispatch(callMemberListForAppAPI());
+  }, []);
 
   return (
     <div className={ApprovalCSS}>
       <div className={ApprovalCSS.square}></div>
       <div className={ApprovalCSS.appContectDiv}>
-
         <ul className={ApprovalCSS.memberChartUl}>
-          {accessors?.dept && accessors?.dept.map((dept) => (
-            <li key={dept.deptCode} /* onClick={() => handleItemClick(dept)} */>
+          {accessors?.dept && accessors?.dept.map(dept => (
+            <li key={dept.deptCode}>
               {dept.deptName}
-              {renderMembers(dept)}
-              <ul>
-                
-              </ul>
+              {accessors?.accessor && accessors.accessor.map(accessor => (
+                accessor.department.deptCode === dept.deptCode && (
+                  <ul key={accessor.job.jobCode}>
+                    <li onClick={() => handleItemClick(dept, accessor.job.jobName)}>
+                      {accessor.job.jobName}
+                      {expandedItems.some(expandedItem => (
+                        expandedItem.dept.deptCode === dept.deptCode &&
+                        expandedItem.jobName === accessor.job.jobName
+                      )) && renderMembers(dept, accessor.job.jobName)}
+                    </li>
+                  </ul>
+                )
+              ))}
             </li>
-
           ))}
         </ul>
       </div>
@@ -59,50 +80,59 @@ function NestedList() {
 
 export default NestedList;
 
+
 // import React, { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { callMemberListForAppAPI } from '../../apis/AppLineAPICalls';
 // import ApprovalCSS from './Approval.module.css';
 
-// const NestedList = () => {
+// function NestedList() {
 //   const [expandedItems, setExpandedItems] = useState([]);
+//   const {data, accessors} = useSelector(state => state.applineReducer);
 //   const dispatch = useDispatch();
-//   const {data} = useSelector(state => state.approvalReducer);
 
+//   //부서에 해당하는 직원 조회
+//   const renderMembers = (dept) => {
+//     if (!accessors) return null; // data가 undefined인 경우 처리
 
-//   const handleItemClick = (item) => {
-//     if (expandedItems.includes(item)) {
-//       setExpandedItems(expandedItems.filter((expandedItem) => expandedItem !== item));
-//     } else {
-//       setExpandedItems([...expandedItems, item]);
-//     }
+//     const members = accessors.accessor.filter(accessor => accessor.department.deptCode === dept.deptCode);
+
+//     if (members.length === 0) return null;
+
+//     return (
+//       <ul>
+//         {members.map(member => (
+//           <li key={member.memberCode}>
+//             {member.job.jobName} - 
+//             {member.memberName}
+//             {renderMembers(member)} {/* 재귀적으로 하위 멤버 출력 */}
+//           </li>
+//         ))}
+//       </ul>
+//     );
 //   };
 
 //   useEffect(() => {
-//     dispatch(callMemberListForAppAPI());
-//   }, []);
+//       dispatch(callMemberListForAppAPI());
+//     }, 
+//     []
+//   );
 
 //   return (
 //     <div className={ApprovalCSS}>
 //       <div className={ApprovalCSS.square}></div>
 //       <div className={ApprovalCSS.appContectDiv}>
-//         <ul> 
-//           {data && data.map((appline) => (
-//             <li key={appline.memberCode} onClick={() => handleItemClick(appline)}>
-//               <div>
-//                 <span>코드: {appline.memberCode}</span>
-//               </div>
-//               <div>
-//                 <span>이름: {appline.memberName}</span>
-//               </div>
-//               {/* {expandedItems.includes(appline) && (
-//                 <ul>
-//                   {appline.department.items.map((item) => (
-//                     <li key={item.deptCode}>{item.deptName}</li>
-//                   ))}
-//                 </ul>
-//               )} */}
+
+//         <ul className={ApprovalCSS.memberChartUl}>
+//           {accessors?.dept && accessors?.dept.map((dept) => (
+//             <li key={dept.deptCode} /* onClick={() => handleItemClick(dept)} */>
+//               {dept.deptName}
+//               {renderMembers(dept)}
+//               <ul>
+                
+//               </ul>
 //             </li>
+
 //           ))}
 //         </ul>
 //       </div>
