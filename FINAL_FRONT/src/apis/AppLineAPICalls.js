c7aed0f1a9bbc93f283b34ad2930d03c28742d83
ehyunseok <yhdaneys@gmail.com>
2023-05-24 21:24:53 +0900

    이현-결재선 인서트 시도중

--- FINAL_FRONT/src/apis/AppLineAPICalls.js
+++ FINAL_FRONT/src/apis/AppLineAPICalls.js
@@ -1,8 +1,25 @@
-import {getApplineMembers, getApplineMember, postAppline} from "../modules/ApplineModule";
+import {getApplineMembers, getApprovalInfo, postAppline} from "../modules/ApplineModule";
 
 const SERVER_IP = `${process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const SERVER_PORT = `${process.env.REACT_APP_RESTAPI_SERVER_PORT}`;
 const PRE_URL = `http://${SERVER_IP}:${SERVER_PORT}/skylift/approval`;
+
+// 결재선 선택을 위한 결재정보 조회
+export const callApprovalInfoForAppAPI = ({appCode}) => {
+
+    const reqeustURL = `${PRE_URL}/registedApp/${appCode}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(reqeustURL).then(res => res.json());
+
+        if(result.status === 200) {
+            console.log('callApprovalInfoForAppAPI result : ', result);
+            dispatch(getApprovalInfo(result));
+        }
+
+    }
+}
 
 
 // 결재선 선택을 위한 직원목록 조회
@@ -12,7 +29,7 @@ export const callMemberListForAppAPI = () => {
 
     return async (dispatch, getState) => {
 
-        const result = await fetch(reqeustURL).then(response => response.json());
+        const result = await fetch(reqeustURL).then(res => res.json());
 
         if(result.status === 200) {
             console.log('callMemberListForAppAPI result : ', result);
@@ -22,26 +39,9 @@ export const callMemberListForAppAPI = () => {
     }
 }
 
-// 결재선 선택을 위한 직원목록 조회
-export const callMemberForAppAPI = ({memberCode}) => {
-
-    const reqeustURL = `${PRE_URL}/selectedMember/${memberCode}`;
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(reqeustURL).then(response => response.json());
-
-        if(result.status === 200) {
-            console.log('callMemberListForAppAPI result : ', result);
-            dispatch(getApplineMember(result));
-        }
-
-    }
-}
-
 // 결재선 등록
-export const callAppLineInsertAPI = (form2) => {
-    const requestURL = `${PRE_URL}/appline`;
+export const callAppLine1InsertAPI = (form) => {
+    const requestURL = `${PRE_URL}/appline1`;
 
     return async (dispatch, getState) => {
         const result = await fetch(requestURL, {
@@ -50,7 +50,7 @@ export const callAppLineInsertAPI = (form2) => {
                 "Content-Type" : "application/json",
                 "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
             },
-            body : JSON.stringify(form2),
+            body : JSON.stringify(form),
         }).then(response => response.json());
 
         if(result.status === 200) {
@@ -59,4 +59,44 @@ export const callAppLineInsertAPI = (form2) => {
         }
     }
 }
+export const callAppLine2InsertAPI = (form) => {
+    const requestURL = `${PRE_URL}/appline2`;
+
+    return async (dispatch, getState) => {
+        const result = await fetch(requestURL, {
+            method: 'POST',
+            headers : {
+                "Content-Type" : "application/json",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify(form),
+        }).then(response => response.json());
+
+        if(result.status === 200) {
+            console.log('[ApprovalAPICalls] :  callAppLineInsertAPI result : ', result);
+            dispatch(postAppline(result));
+        }
+    }
+}
+export const callAppLine3InsertAPI = (form) => {
+    const requestURL = `${PRE_URL}/appline3`;
+
+    return async (dispatch, getState) => {
+        const result = await fetch(requestURL, {
+            method: 'POST',
+            headers : {
+                "Content-Type" : "application/json",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify(form),
+        }).then(response => response.json());
+
+        if(result.status === 200) {
+            console.log('[ApprovalAPICalls] :  callAppLineInsertAPI result : ', result);
+            dispatch(postAppline(result));
+        }
+    }
+}
+
+//
 
